33fefdb790d931347ee80253baf6e1516519b3e4
Jorge Manzano <manzanoglez1995@gmail.com>
2020-08-10 11:18:02 -0500

    subiendo docs

---
+++ docs/precache-manifest.fdae7fbcf292df98bd625339ef99aa7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7fa2eb99381f4bfa5aeb77c028f0739",
+    "url": "/index.html"
+  },
+  {
+    "revision": "355f09dcfbd2c406fea1",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "19fe25ab9d938f49025b",
+    "url": "/static/css/main.05bb529b.chunk.css"
+  },
+  {
+    "revision": "355f09dcfbd2c406fea1",
+    "url": "/static/js/2.175e6906.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.175e6906.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19fe25ab9d938f49025b",
+    "url": "/static/js/main.e815d113.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
